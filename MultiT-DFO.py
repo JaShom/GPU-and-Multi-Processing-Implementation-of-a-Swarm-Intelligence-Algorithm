fe4f3949b125cb675f91d397cd9a451281813535
Awwal <jubral14@gmail.com>
2022-04-25 21:09:23 +0100

    completed

--- MultiT-DFO.py
+++ MultiT-DFO.py
@@ -15,7 +15,7 @@ Park Row, London SE10 9LS, U.K.
 Reference to origianl paper:
 Mohammad Majid al-Rifaie (2014), Dispersive Flies Optimisation, Proceedings of the 2014 Federated Conference on Computer Science and Information Systems, 535--544. IEEE.
 
-	@inproceedings{FedCSIS_2014,
+    @inproceedings{FedCSIS_2014,
 		author={Mohammad Majid al-Rifaie},
 		pages={535--544},
 		title={Dispersive Flies Optimisation},
@@ -29,25 +29,22 @@ Mohammad Majid al-Rifaie (2014), Dispersive Flies Optimisation, Proceedings of t
 # If you do decide to run this to check the time ran, I'd advise you watch a 10-20 min video of something to pass time
 
 import numpy as np
-import matplotlib as plt
-import pandas as pd
 from time import perf_counter
-from math import floor, log10
-import numba
-from numba import jit
-import pycuda.driver as cuda
-import pycuda.autoinit
-from pycuda.compiler import SourceModule
-from pycuda import gpuarray
+import threading as td
+import multiprocessing as mp
+from multiprocessing import pool
+import concurrent.futures
+import os
+import sys
 
-count = 0
+open('MultiFitness.txt', 'w').close()
 
 
-def scientific(x, n):
-    """Represent a float in scientific notation.
-	This function is merely a wrapper around the 'e' type flag in the
-	formatting specification.
-	"""
+def scientific(x, n):  # Taken from https://github.com/corriander/python-sigfig/blob/dev/sigfig/sigfig.py
+    # """Represent a float in scientific notation.
+    # This function is merely a wrapper around the 'e' type flag in the
+    # formatting specification.
+    # """
     n = int(n)
     x = float(x)
 
@@ -56,67 +53,22 @@ def scientific(x, n):
     return ''.join(('{:.', str(n - 1), 'e}')).format(x)
 
 
-# mod = SourceModule(
-#     """
-#         float f(float x[], int D) { // x IS ONE FLY AND D IS DIMENSION
-#             float sum=0;
-#             for(int i=0; i<D; i++)
-#                 sum=sum+x[i]*x[i];
-#             return sum;
-#         }
-#     """)
-# updateFly = SourceModule(
-#     """
-#     // UPDATE EACH FLY INDIVIDUALLY
-#     for(int i=0; i<N; i++) {
-#         // ELITIST STRATEGY (i.e. DON'T UPDATE BEST FLY)
-#         if (i==s) continue;
-#
-#             // FIND BEST NEIGHBOUR FOR EACH FLY
-#             int left; int right; int bNeighbour;
-#             left=(i-1)%N; right=(i+1)%N; // INDICES: LEFT & RIGHT FLIES
-#             if (device_flies[right]<device_flies[left]) bNeighbour = right;
-#             else bNeighbour = left;
-#
-#             // UPDATE EACH DIMENSION SEPARATELY
-#             for (int d=0; d<D; d++) {
-#                 if (r() < delta) { // DISTURBANCE MECHANISM
-#                     X[i][d] = lowerB[d] + r()*(upperB[d]-lowerB[d]); continue;
-#                 }
-#
-#                 // UPDATE EQUATION
-#                 X[i][d] = X[bNeighbour][d] + r()*( X[s][d]- X[i][d] );
-#
-#                 // OUT OF BOUND CONTROL
-#                 if (X[i][d] < lowerB[d] or X[i][d] > upperB[d])
-#                 X[i][d] = lowerB[d] + r()*(upperB[d]-lowerB[d]);
-#             }
-#         }
-#     """
-# )
-# sphereFunc = mod.get_function("f")
-
-
-
-# with tf.device('/GPU:0'):
-@jit(target_backend="gpu")
-def Sphere(x):  # x IS A VECTOR REPRESENTING ONE FLY, SPHEREFUNCTION
+# FITNESS FUNCTION (SPHERE FUNCTION)
+def sphere(x):  # x IS A VECTOR REPRESENTING ONE FLY. SPHERE FUNCTION with bounds ∈ [-100, 100]
     sums = 0.0
     for i in range(len(x)):
         sums = sums + np.power(x[i], 2)
     return sums
 
 
-@jit(target_backend="gpu")
-def rastrigin(x):  # RastriginFunction ∈ [-5.12, 5.12]
+def rastrigin(x):  # Rastrigin Function, ∈ [-5.12, 5.12]
     sums = 0.0
     for i in range(len(x)):
         sums += x[i] ** 2 - (10 * np.cos(2 * np.pi * x[i])) + 10
     return sums
 
 
-@jit(target_backend="gpu")
-def schwefel_1_2(x):  # ∈ [−100,100]
+def schwefel_1_2(x):  # Schwefel 1.2 Function, ∈ [−100,100]
     # x = np.array(x)
     sums = 0.0
     for i in range(len(x)):
@@ -124,8 +76,7 @@ def schwefel_1_2(x):  # ∈ [−100,100]
     return sums
 
 
-@jit(target_backend="gpu")
-def rosenbrock(x):  # ∈ [-5, 10] but may be restricted to [-2.048, 2.048]
+def rosenbrock(x):  # Rosenbrock Function, ∈ [-5, 10] but may be restricted to [-2.048, 2.048]
     sums = 0.0
     for i in range(len(x) - 1):
         xn = x[i + 1]
@@ -134,10 +85,7 @@ def rosenbrock(x):  # ∈ [-5, 10] but may be restricted to [-2.048, 2.048]
     return sums
 
 
-# Check if value are correct later. Mean was a bit large at least 45 min wait time tho.
-
-@jit(target_backend="gpu")
-def ackley(x):  # ∈ [-32.768, 32.768]
+def ackley(x):  # Ackley Function, ∈ [-32.768, 32.768]
     sum = 0.0
     sum2 = 0.0
     for c in x:
@@ -147,8 +95,7 @@ def ackley(x):  # ∈ [-32.768, 32.768]
     return -20.0 * np.exp(-0.2 * np.sqrt(sum / n)) - np.exp(sum2 / n) + 20 + np.exp(1)
 
 
-@jit(target_backend="gpu")
-def griewank(x):  # ∈ [-600, 600]
+def griewank(x):  # Griewank Function, ∈ [-600, 600]
     p1 = 0
     for i in range(len(x)):
         p1 += x[i] ** 2
@@ -158,8 +105,7 @@ def griewank(x):  # ∈ [-600, 600]
     return 1 + (float(p1) / 4000.0) - float(p2)
 
 
-@jit(target_backend="gpu")
-def goldstein(x):  # ∈ [-2, 2]
+def goldstein(x):  # Goldstein Function, ∈ [-2, 2]
     if len(x) <= 2:
         return goldsteinAid(0, x)
     else:
@@ -170,10 +116,9 @@ def goldstein(x):  # ∈ [-2, 2]
     return total
 
 
-@jit(target_backend="gpu")
-def goldsteinAid(i, x):
+def goldsteinAid(i, x):  # Main component of the Goldstein Function
     x1 = x[i]
-    x2 = x[(i + 1) % len(x)]
+    x2 = x[(i + 1) % len(x)]  # Two flies are taken to be used
     eq1a = np.power(x1 + x2 + 1, 2)
     eq1b = 19 - 14 * x1 + 3 * np.power(x1, 2) - 14 * x2 + 6 * x1 * x2 + 3 * np.power(x2, 2)
     eq1 = 1 + eq1a * eq1b
@@ -183,8 +128,7 @@ def goldsteinAid(i, x):
     return eq1 * eq2
 
 
-@jit(target_backend="gpu")
-def camel6(x):  # x1 ∈ [-3, 3], x2 ∈ [-2, 2]
+def camel6(x):  # Six-Hump Camel-Back Function, x1 ∈ [-3, 3] and x2 ∈ [-2, 2] so decided to use ∈ [-5, 5]
     if len(x) <= 2:
         return camel6Aid(0, x)
     else:
@@ -195,18 +139,16 @@ def camel6(x):  # x1 ∈ [-3, 3], x2 ∈ [-2, 2]
         return total
 
 
-@jit(target_backend="gpu")
-def camel6Aid(i, x):
+def camel6Aid(i, x):  # Main component of the Camel-Back Function
     x1 = x[i]
-    x2 = x[(i + 1) % len(x)]
+    x2 = x[(i + 1) % len(x)]  # Two flies are also needed
     part1 = (4 - 2.1 * np.power(x1, 2) + (np.power(x1, 4) / 3)) * np.power(x1, 2)
     part2 = x1 * x2
     part3 = (- 4 + 4 * np.power(x2, 2))
     return part1 + part2 + part3
 
 
-@jit(target_backend="gpu")
-def lunaceksBiRastrigin(x):  # prep for death # ∈ [-5.12,5.12]
+def lunaceksBiRastrigin(x):  # Lunaceks Bi-Rastrigin Function ∈ [-5.12,5.12]
     sum = 0.0
     sum1 = 0.0
     sum2 = 0.0
@@ -221,8 +163,7 @@ def lunaceksBiRastrigin(x):  # prep for death # ∈ [-5.12,5.12]
     return min(sum, d * len(x) + s * sum1) + 10 * sum2
 
 
-@jit(target_backend="gpu")
-def schafferN06(x):  # prep for death
+def schafferN06(x):  # Schaffer N0 6 Function ∈ [-100,100]
     if len(x) <= 2:
         return schafferAid(0, x)
     else:
@@ -233,29 +174,44 @@ def schafferN06(x):  # prep for death
         return total
 
 
-@jit(target_backend="gpu")
-def schafferAid(i, x):
+def schafferAid(i, x):  # Main component of Schaffer N06 Function
     x1 = x[i]
-    y1 = x[(i + 1) % len(x)]
+    y1 = x[(i + 1) % len(x)]  # Uses two flies
     xysqrd = x1 ** 2 + y1 ** 2
     return 0.5 + (np.sin(np.sqrt(xysqrd)) - 0.5) / (1 + 0.001 * xysqrd) ** 2
 
 
-@jit(target_backend="gpu")
-def shiftedRosenbrok(x):
+def shiftedRastrigin(x):  # Shifted Rastrigin Function, ∈ [-5, 5]
+    summ = 0.0
+    sum1 = 0.0
+    fopt = 100
+    for i in range(len(x)):
+        summ = summ + (np.power(x[i], 2) - 10 * np.cos(2 * np.pi * x[i]) + 10)  # The original Rastrgin function is -
+        # used to get the optimal fly, so it can be used for shifting the function
+
+        xopt = 0.0512 * (summ - x[i])  # The optimal fly is stored and ready to be used again
+    return sum1 + (np.power(xopt, 2) - 10 * np.cos(2 * np.pi * xopt) + 10 + fopt)  # Shifted Rastrign function is -
+    # executed on return here
+
+
+def shiftedRosenbrock(x):  # Shifted Rosenbrock Function, ∈ [-2, 2]
     sums = 0.0
     sums1 = 0.0
     fopt = 100
     for i in range(len(x) - 1):
         xn = x[i + 1]
-        new = 100 * np.power(np.power(x[i], 2) - xn, 2) + np.power(x[i] - 1, 2)
+        new = 100 * np.power(np.power(x[i], 2) - xn, 2) + np.power(x[i] - 1, 2)  # The original Rosenbrock function is
+        # used to get the optimal fly, so it can later be used for shifting
+
         sums = sums + new
         xopt = 0.02048 * (sums - x[i]) + 1
-    return sums1 + 100 * np.power(np.power(xopt, 2) - xopt, 2) + np.power(xopt - 1, 2)
+    return sums1 + 100 * np.power(np.power(xopt, 2) - xopt, 2) + np.power(xopt - 1, 2)  # Shifted Rosenbrock function is
+    # executed on return here
 
 
-lis = []
-t1_start = perf_counter()
+t0 = perf_counter()
+# file = open("MultiFitness.txt", "r+")
+
 N = 100  # POPULATION SIZE
 D = 30  # DIMENSIONALITY
 delta = 0.001  # DISTURBANCE THRESHOLD
@@ -263,13 +219,13 @@ maxIterations = 3100  # ITERATIONS ALLOWED
 lowerB = [-100] * D  # LOWER BOUND (IN ALL DIMENSIONS)
 upperB = [100] * D  # UPPER BOUND (IN ALL DIMENSIONS)
 
-for i in range(1):
-    count += 1
-    print(f'This is trial {count}')
+
+def multi():
+    lis = []
     # INITIALISATION PHASE
-    X = np.empty([N, D], dtype=np.float32)  # EMPTY FLIES ARRAY OF SIZE: (N,D)
-    device_flies = [None] * N  # EMPTY device_flies ARRAY OF SIZE N
-    device_flies = gpuarray.to_gpu(device_flies)
+    X = np.empty([N, D])  # EMPTY FLIES ARRAY OF SIZE: (N,D)
+    fitness = [None] * N  # EMPTY FITNESS ARRAY OF SIZE N
+
     # INITIALISE FLIES WITHIN BOUNDS
     for i in range(N):
         for d in range(D):
@@ -278,13 +234,12 @@ for i in range(1):
     # MAIN DFO LOOP
     for itr in range(maxIterations):
         for i in range(N):  # EVALUATION
-         #   gpuSphere = Sphere.to_gpu()
-            device_flies = Sphere(X[i,])
-        s = np.argmin(device_flies)  # FIND BEST FLY
+            fitness[i] = sphere(X[i,])
+        s = np.argmin(fitness)  # FIND BEST FLY
 
         if itr % 100 == 0:  # PRINT BEST FLY EVERY 100 ITERATIONS
             print("Iteration:", itr, "\tBest fly index:", s,
-                  "\tdevice_flies value:", device_flies[s])
+                  "\tFitness value:", fitness[s])
 
         # TAKE EACH FLY INDIVIDUALLY
         for i in range(N):
@@ -293,7 +248,7 @@ for i in range(1):
             # FIND BEST NEIGHBOUR
             left = (i - 1) % N
             right = (i + 1) % N
-            bNeighbour = right if device_flies[right] < device_flies[left] else left
+            bNeighbour = right if fitness[right] < fitness[left] else left
 
             for d in range(D):  # UPDATE EACH DIMENSION SEPARATELY
                 if np.random.rand() < delta:
@@ -307,27 +262,48 @@ for i in range(1):
                 if X[i, d] < lowerB[d] or X[i, d] > upperB[d]:
                     X[i, d] = np.random.uniform(lowerB[d], upperB[d])
 
-    for i in range(N): device_flies[i] = Sphere(X[i,])  # EVALUATION
-    s = np.argmin(device_flies)  # FIND BEST FLY
-    hostFlies = device_flies.get()
-    lis.append(hostFlies[s])  # Apply 30 best flies into list
+    for i in range(N): fitness[i] = sphere(X[i,])  # EVALUATION
+    s = np.argmin(fitness)  # FIND BEST FLY
+    lis.append(fitness[s])
 
-t1_stop = perf_counter()
-print("\nFinal best device_flies:\t", hostFlies[s])
-print("\nBest fly position:\n", X[s,])
+    with open("MultiFitness.txt", "a+") as f:
+        f.write(f"{fitness[s]}\n")  # :.50f}\n")
+        f.close()
 
-print("Time elapsed:", t1_stop - t1_start)
-minimum = X[s,].min()
-maximum = X[s,].max()
-Fmean = X[s,].mean()
-standardDev = X[s,].std()
-print("The min is: ", minimum, "\nThe Max is: ", maximum, "\nThe Mean is: ", Fmean, "\nThe Standard Deviation is: ",
-      standardDev)
+    print("\nFinal best fitness:\t", fitness[s])
+    print("\nBest fly position:\n", X[s,])
+    minimum = X[s,].min()
+    maximum = X[s,].max()
+    Fmean = X[s,].mean()
+    standardDev = X[s,].std()
+    print("The min is: ", minimum, "\nThe Max is: ", maximum, "\nThe Mean is: ", Fmean, "\nThe Standard Deviation is: ",
+          standardDev)
 
-print("\n Time elapsed: ", (t1_stop - t1_start))
-print("This is the best fly after 30 trials:", lis, "\nMin = ", min(lis), "\nMax = ", max(lis), "\nMedian = ",
-      np.median(lis),
-      "\nMean = ", np.mean(lis), "\nStandard deviation = ", np.std(lis), "\nCounter says ", count)
-fimean = np.mean(lis)
-standardDev = np.std(lis)
-print("Mean: ", scientific(fimean, 3), "Standard deviation: ", scientific(standardDev, 3))
+
+if __name__ == "__main__":
+    threads = []
+    tStart = perf_counter()
+    for _ in range(30):
+        t1 = td.Thread(target=multi)
+        t1.daemon = True
+        threads.append(t1)
+
+    for i in range(30):
+        threads[i].start()
+    for i in range(30):
+        threads[i].join()
+
+    # for thread in threads:
+    #     thread.join()
+    tEnd = perf_counter()
+    newLis = np.loadtxt('MultiFitness.txt', delimiter="\n")
+
+    print(newLis)
+    print("These are the best fies after 30 trials:", newLis, f"\nMin = ", min(newLis),
+          "\nMax = ", max(newLis), "\nMedian = ", np.median(newLis), "\nMean = ", np.mean(newLis),
+          "\nStandard deviation = ", np.std(newLis), "\nCounter says ")
+
+    print("Time elapsed: ", tEnd - tStart)
+    fMean = np.mean(newLis)
+    standardDev = np.std(newLis)
+    print("Mean: ", scientific(fMean, 3), "Standard deviation: ", scientific(standardDev, 3))
